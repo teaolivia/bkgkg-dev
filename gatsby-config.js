7f60ed8c48d4a02961b410373be6a6fe6255a25a
Theaolivia <theaolivia@Theaolivias-iMac.local>
2019-05-26 22:01:03 +0700

    color theme chosen

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `BKGKG`,
+    description: `Website BKGKG`,
+    author: `@teaolivia`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,12 +21,24 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f8cd65`,
+        theme_color: `#f8cd65`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bold-text.png`, // This path is relative to the root of the site.
       },
     },
+    // {
+    //   resolve: `gatsby-source-wordpress`,
+    //   options: {
+    //     // your wordpress source
+    //     baseUrl: `wpexample.com`,
+    //     protocol: `https`,
+    //     // is it hosted on wordpress.com, or self-hosted?
+    //     hostingWPCOM: false,
+    //     // does your site use the Advanced Custom Fields Plugin?
+    //     useACF: false
+    //   }
+    // }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
